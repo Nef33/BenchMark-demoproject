d18110c819538908b7fd1168e48788ee098f2d6a
nef <nefisekyrk@gmail.com>
2023-11-05 13:28:31 -0600

    reporting plugin added

--- src/test/java/com/rsi/runners/CukesRunner.java
+++ src/test/java/com/rsi/runners/CukesRunner.java
@@ -7,10 +7,10 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
-
-                "html:target/cucumber-reports.html",
-                "rerun:target/rerun.txt",
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+                "json:target/cucumber.json",
+                "html:target/cucumber/report.html",
+                "junit:target/junit/junit-report.xml",
+                "rerun:target/rerun.txt"
         },
         features = "src/test/resources/features",
         glue = "com/rsi/step_definitions",
